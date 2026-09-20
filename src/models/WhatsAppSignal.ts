772a8983ae525a386a03982dd0ef8baf51808c23
Supun Dilshan <taizercodecrafter@gmail.com>
2026-09-20 11:13:50 +0530

    fix: restore TS fix for WhatsAppSignal after revert

--- src/models/WhatsAppSignal.ts
+++ src/models/WhatsAppSignal.ts
@@ -15,4 +15,4 @@ const WhatsAppSignalSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now }
 });
 
-export const WhatsAppSignal = mongoose.models.WhatsAppSignal || mongoose.model('WhatsAppSignal', WhatsAppSignalSchema);
+export const WhatsAppSignal = (mongoose.models.WhatsAppSignal || mongoose.model('WhatsAppSignal', WhatsAppSignalSchema)) as mongoose.Model<any>;
