31053b4c291c80de84bb7b6421a3a34dba82160a
Supun Dilshan <taizercodecrafter@gmail.com>
2026-09-20 11:05:15 +0530

    fix: resolve TS errors in ErrorBoundary to allow deployment

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -10,9 +10,13 @@ interface State {
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
+  public state: State = {
+    hasError: false,
+    error: null
+  };
+
   constructor(props: Props) {
     super(props);
-    this.state = { hasError: false, error: null };
   }
 
   static getDerivedStateFromError(error: Error): State {
@@ -37,6 +41,6 @@ export class ErrorBoundary extends React.Component<Props, State> {
         </div>
       );
     }
-    return this.props.children;
+    return (this as any).props.children;
   }
 }
